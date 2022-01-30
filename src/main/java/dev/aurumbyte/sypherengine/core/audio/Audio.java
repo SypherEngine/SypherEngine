6aee1502cf0c02b5e70693427e7dab64e6f3b709
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-01-30 16:48:50 +0530

    add button ui support

---
+++ src/main/java/dev/aurumbyte/sypherengine/core/audio/Audio.java
@@ -0,0 +1,119 @@
+package dev.aurumbyte.sypherengine.core.audio;
+
+import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class Audio {
+    /*
+    String file;
+    Media media;
+    MediaPlayer mediaPlayer;
+
+    public Audio(String file){
+        this.file = file;
+        this.media = new Media(new File(file).toURI().toString());
+        this.mediaPlayer = new MediaPlayer(media);
+    }
+
+    public void play() {
+        mediaPlayer.play();
+    }
+
+    public void stop() {
+        mediaPlayer.stop();
+    }
+
+    public void loop() {
+        mediaPlayer.setOnEndOfMedia(() -> {
+            mediaPlayer.seek(Duration.ZERO);
+            mediaPlayer.play();
+        });
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public Media getMedia() {
+        return media;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
+    }
+
+    public static class Clip {
+        String file;
+        AudioClip audioClip;
+
+        public Clip(String file){
+            this.file = file;
+            this.audioClip = new AudioClip(new File(file).toURI().toString());
+        }
+
+        public void play() {
+            audioClip.play();
+        }
+
+        public void loop() {
+            audioClip.play();
+            if(!audioClip.isPlaying()) audioClip.play();
+        }
+
+        public void stop() {
+            audioClip.stop();
+        }
+    }
+
+     */
+    private Clip clip;
+    private FloatControl controller;
+
+    public Audio(String path){
+        try {
+            InputStream audio = Audio.class.getResourceAsStream(path) ;
+            InputStream bufferedAudio = new BufferedInputStream(Objects.requireNonNull(audio));
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedAudio);
+            AudioFormat format = audioInputStream.getFormat();
+            AudioFormat decodedFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED,
+                    format.getSampleRate(),
+                    16,
+                    format.getChannels(),
+                    format.getChannels() * 2,
+                    format.getSampleRate(),
+                    false
+            );
+
+            AudioInputStream decodedAudioStream = AudioSystem.getAudioInputStream(decodedFormat, audioInputStream);
+
+            clip = AudioSystem.getClip();
+            clip.open(decodedAudioStream);
+            controller = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play(){
+        if(clip == null) return;
+
+
+        stop();
+        clip.setFramePosition(0);
+
+        while(!clip.isRunning()){
+            clip.start();
+        }
+    }
+
+    public void stop(){ if(clip.isRunning()) clip.stop(); }
+    public void close(){ stop(); clip.drain(); clip.close(); }
+    public void loop(){ clip.loop(Clip.LOOP_CONTINUOUSLY); play(); }
+    public void setVolume(float volume){ controller.setValue(volume); }
+    public boolean isRunning(){ return clip.isRunning(); }
+}
---
+++ src/main/java/dev/aurumbyte/sypherengine/core/logic/GameManager.java
@@ -0,0 +1,44 @@
+package dev.aurumbyte.sypherengine.core.logic;
+
+import dev.aurumbyte.sypherengine.components.scene.Scene;
+import dev.aurumbyte.sypherengine.core.SypherEngine;
+import dev.aurumbyte.sypherengine.core.graphics.Renderer;
+
+public abstract class GameManager extends Scene {
+    Scene currentScene = this;
+
+    public void gameInit(SypherEngine engine){
+        engine.getRenderer().addEntities(currentScene.entities);
+        currentScene.init(engine);
+    }
+
+    public void gameUpdate(SypherEngine engine, float deltaTime){
+        currentScene.entities.forEach(entity -> {
+            if(entity.isDead()) currentScene.entities.remove(entity);
+            entity.update(engine, deltaTime);
+        });
+
+        currentScene.update(engine, deltaTime);
+    }
+
+    public void gameRender(Renderer renderer){
+        currentScene.entities.forEach(entity -> {
+            renderer.transformContext(entity);
+            entity.render(renderer);
+        });
+
+        currentScene.render(renderer);
+    }
+
+    public abstract void init(SypherEngine engine);
+    public abstract void update(SypherEngine engine, float deltaTime);
+    public abstract void render(Renderer renderer);
+
+    public void setCurrentScene(Scene currentScene) {
+        this.currentScene = currentScene;
+    }
+
+    public Scene getCurrentScene() {
+        return currentScene;
+    }
+}
