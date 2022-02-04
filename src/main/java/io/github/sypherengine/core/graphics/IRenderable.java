2a18263109ed22e5c26c5d5e9b3d9910cf8ac760
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-02-04 19:40:29 +0530

    Delete src/main/java directory

--- src/main/java/io/github/sypherengine/core/graphics/IRenderable.java
+++
@@ -1,8 +0,0 @@
-package io.github.sypherengine.core.graphics;
-
-import io.github.sypherengine.core.SypherEngine;
-
-public interface IRenderable {
-    void update(SypherEngine engine);
-    void render(SypherEngine engine);
-}
