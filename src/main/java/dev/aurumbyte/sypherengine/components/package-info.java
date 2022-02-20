f415cbee3d35fd0d4d63174c4c10f82c5f37d097
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-02-20 19:06:51 +0530

    Delete src/main/java directory

--- src/main/java/dev/aurumbyte/sypherengine/components/package-info.java
+++
@@ -1,1 +0,0 @@
-package dev.aurumbyte.sypherengine.components;
--- src/main/java/dev/aurumbyte/sypherengine/logging/logUtils/LoggerLevel.java
+++
@@ -1,5 +0,0 @@
-package dev.aurumbyte.sypherengine.logging.logUtils;
-
-public enum LoggerLevel {
-    TRACE, DEBUG, ERROR, FATAL, INFO, WARN, OFF;
-}
