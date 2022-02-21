e5f3f3b56756169e2bbd89e11ecd28d3e7ccb0c1
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-02-21 18:23:49 +0530

    Add files via upload

---
+++ src/main/java/dev/aurumbyte/sypherengine/config/EngineConfig.java
@@ -0,0 +1,43 @@
+package dev.aurumbyte.sypherengine.config;
+
+import dev.aurumbyte.sypherengine.math.Vector2;
+
+public class EngineConfig {
+    String title;
+    int width = 1280, height = 720;
+
+    float fixedUpdate = 60;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWindowResolution(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getScreenHeight() {
+        return height;
+    }
+
+    public int getScreenWidth() {
+        return width;
+    }
+
+    public void setFixedUpdate(float fixedUpdate) {
+        this.fixedUpdate = fixedUpdate;
+    }
+
+    public float getFixedUpdate() {
+        return fixedUpdate;
+    }
+
+    public Vector2 getWindowResolution(){
+        return new Vector2(width, height);
+    }
+}
