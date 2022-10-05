cd9a71ded32e33d810ed00280cdb000fc56a3027
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-10-05 13:24:02 +0530

    Delete examples directory

--- examples/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module io.github.sypherengine.examples {
-    requires io.github.sypherengine.core;
-    requires io.github.sypherengine.util;
-    requires io.github.sypherengine.ecs;
-    requires javafx.base;
-    requires javafx.graphics;
-}
