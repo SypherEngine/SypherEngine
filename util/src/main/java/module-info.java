0557f08e560ae5743d20a16008a4c65060066712
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-10-05 13:26:09 +0530

    Delete util directory

--- util/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module io.github.sypherengine.util {
-    exports dev.aurumbyte.sypherengine.util.logging;
-    exports dev.aurumbyte.sypherengine.util.logging.logUtils;
-    exports dev.aurumbyte.sypherengine.util.math;
-}
