3c097ffe9748f02a456f5610542a08987984c24a
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-10-05 14:15:46 +0530

    Delete physics directory

--- physics/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module io.github.sypherengine.physics {
-    requires io.github.sypherengine.util;
-    requires io.github.sypherengine.ecs;
-
-    exports dev.aurumbyte.sypherengine.physics.forces;
-    exports dev.aurumbyte.sypherengine.physics;
-    exports  dev.aurumbyte.sypherengine.physics.rigidbody;
-    exports dev.aurumbyte.sypherengine.physics.primitives;
-}
